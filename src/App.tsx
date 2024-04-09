14f0ae53fd1ae1a8bcd12f5670cdc26b654ec799
Russell Johnson <russellmax86@gmail.com>
2024-04-09 11:07:22 -0600

    update initial state for light mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import UserSearch from './UserSearch';
-
+import { keepMode } from './utils/theme';
 function App() {
 
+  useEffect(() => {
+    keepMode();
+  }, []);
   
   return (
     <div className={"bg-main h-full flex align-center"}>
